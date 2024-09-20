3c4b3e479250d36d681e730ae0fdaa628d09e7a9
Serg18353 <mikholenok@yandex.ru>
2024-09-20 16:16:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+a = int(input(print('Введите число')))
+print(a)
+a = 18
+s = []
+n = 1
+m = 2
+while a == int(a):
+    if a % (n + m) == 0:
+        s.append([n, m])
+    if n + m < a:
+        m += 1
+    else:
+        n += 1
+        m = n + 1
+    if a//2  == n - 1:
+        break
+print(*s)
